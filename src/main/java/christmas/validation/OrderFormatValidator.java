2c7b2143233006c3e14d7a129449a390117f88a2
eunhee <dmsgml9903@gmail.com>
2023-11-10 02:32:01 +0900

    fix(OrderFormatValidator): 정규식 오류 수정 및 예외가 발생하면 에러 문구를 보여주도록 수정
    아래 내용을 수정:
    - 예외 발생시 에러 문구가 나오도록 수정
    - 문자열-숫자 형식을 정규 표현식으로 표현하도록 수정

--- src/main/java/christmas/validation/OrderFormatValidator.java
+++ src/main/java/christmas/validation/OrderFormatValidator.java
@@ -1,12 +1,17 @@
 package christmas.validation;
 
+import static christmas.constant.ErrorMessage.CONSECUTIVE_DELIMITER_NOT_ALLOWED;
+import static christmas.constant.ErrorMessage.DELIMITER_BOTH_ENDS_NOT_ALLOWED;
+import static christmas.constant.ErrorMessage.WRONG_ORDER_FORMAT;
+
 import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OrderFormatValidator {
 
     private static final String DELIMITER = ",";
-    private static final String ORDER_FORMAT = "%s-%d";
+    private static final String ORDER_FORMAT = "^[가-힣]+-[0-9]+$";
 
     public void validateDelimiterFormat(String orders) {
         validateBothEndsDelimiter(orders);
@@ -17,20 +22,22 @@ public class OrderFormatValidator {
 
     private void validateBothEndsDelimiter(String orders) {
         if (orders.startsWith(DELIMITER) || orders.endsWith(DELIMITER)) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException(DELIMITER_BOTH_ENDS_NOT_ALLOWED.toString());
         }
     }
 
     private void validateConsecutiveDelimiter(String orders) {
         if (orders.contains(DELIMITER + DELIMITER)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(CONSECUTIVE_DELIMITER_NOT_ALLOWED.toString());
         }
 
     }
 
     private void validateOrderFormat(String order) {
-        if (!Pattern.matches(order, ORDER_FORMAT)) {
-            throw new IllegalArgumentException();
+        Pattern pattern = Pattern.compile(ORDER_FORMAT);
+        Matcher matcher = pattern.matcher(order);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(WRONG_ORDER_FORMAT.toString());
         }
     }
 
