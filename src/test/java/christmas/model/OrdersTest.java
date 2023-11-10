52618fc653bccea5be2047ac56c0215301e2a406
eunhee <dmsgml9903@gmail.com>
2023-11-10 16:57:28 +0900

    test(OrdersTest): 주어진 날짜가 주문일보다 빠른지 느린지를 확인하는 함수 테스트

--- src/test/java/christmas/model/OrdersTest.java
+++ src/test/java/christmas/model/OrdersTest.java
@@ -13,10 +13,21 @@ import christmas.model.menu.Menu;
 import java.time.LocalDate;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class OrdersTest {
+
+    Orders createValidOrders(LocalDate orderDate) {
+        Map<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+        orderMenus.put(BUTTON_MUSHROOM_SOUP, 2);
+        orderMenus.put(RED_WINE, 1);
+        orderMenus.put(T_BONE_STEAK, 1);
+        return new Orders(orderMenus, orderDate);
+    }
 
     @DisplayName("메뉴당 구매 개수가 0개면 예외가 발생한다.")
     @Test
@@ -84,4 +95,57 @@ class OrdersTest {
         assertThat(totalPrice).isEqualTo(127000);
     }
 
+    @DisplayName("주문일이 주어진 날짜 이전이면 거짓을 반환한다.")
+    @Test
+    void orderDateBeforeDateReturnFalse() {
+        Orders validOrders = createValidOrders(LocalDate.of(2023, 11, 1));
+        LocalDate date = LocalDate.of(2023, 12, 1);
+
+        Boolean isBeforeDate = validOrders.notBefore(date);
+
+        assertThat(isBeforeDate).isFalse();
+    }
+
+    static Stream<LocalDate> createOrderDate() {
+        return Stream.of(
+                LocalDate.of(2023, 12, 11),
+                LocalDate.of(2023, 12, 1),
+                LocalDate.of(2023, 12, 31)
+        );
+    }
+
+    @DisplayName("주문일이 주어진 날짜 이후거나 같으면 참을 반환한다.")
+    @ParameterizedTest
+    @MethodSource("createOrderDate")
+    void orderDateNotBeforeDateReturnTrue(LocalDate orderDate) {
+        Orders validOrders = createValidOrders(orderDate);
+        LocalDate date = LocalDate.of(2023, 12, 1);
+
+        Boolean isBeforeDate = validOrders.notBefore(date);
+
+        assertThat(isBeforeDate).isTrue();
+    }
+
+    @DisplayName("주문일이 주어진 날짜 이후면 거짓을 반환한다.")
+    @Test
+    void orderDateAfterDateReturnFalse() {
+        Orders validOrders = createValidOrders(LocalDate.of(2024, 1, 1));
+        LocalDate date = LocalDate.of(2023, 12, 31);
+
+        Boolean isBeforeDate = validOrders.notAfter(date);
+
+        assertThat(isBeforeDate).isFalse();
+    }
+
+    @DisplayName("주문일이 주어진 날짜 이전이나 같으면 참을 반환한다.")
+    @ParameterizedTest
+    @MethodSource("createOrderDate")
+    void orderDateNotAfterDateReturnTrue(LocalDate orderDate) {
+        Orders validOrders = createValidOrders(orderDate);
+        LocalDate date = LocalDate.of(2023, 12, 31);
+
+        Boolean isBeforeDate = validOrders.notAfter(date);
+
+        assertThat(isBeforeDate).isTrue();
+    }
 }
