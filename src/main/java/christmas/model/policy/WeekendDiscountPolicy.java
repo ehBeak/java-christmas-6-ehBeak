73cc3808c499d426ff078bd29f6cfd2d4c027f71
eunhee <dmsgml9903@gmail.com>
2023-11-11 03:59:54 +0900

    refactor(WeekendDiscountPolicy): 메인 메뉴에 대한 할인이므로 DISCOUNT_PRICE_PER_MAIN으로 수정

--- src/main/java/christmas/model/policy/WeekendDiscountPolicy.java
+++ src/main/java/christmas/model/policy/WeekendDiscountPolicy.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
 
 public class WeekendDiscountPolicy {
 
-    private static final Integer DISCOUNT_PRICE_PER_DESSERT = 2023;
+    private static final Integer DISCOUNT_PRICE_PER_MAIN = 2023;
     private static final LocalDate START_DATE = LocalDate.of(2023, 12, 1);
     private static final LocalDate END_DATE = LocalDate.of(2023, 12, 31);
 
     public Integer calculateDiscountPrice(Orders orders) {
         Integer totalDiscount = 0;
         if (isEventPeriod(orders) && isWeekend(orders)) {
-            totalDiscount += orders.findMainCount() * DISCOUNT_PRICE_PER_DESSERT;
+            totalDiscount += orders.findMainCount() * DISCOUNT_PRICE_PER_MAIN;
         }
         return totalDiscount;
     }
