25f76f4d228693951454d2c7c1de511e45ace008
eunhee <dmsgml9903@gmail.com>
2023-11-12 04:55:12 +0900

    refactor(Customer): 주문한 내역에 혜택을 받을 수 있는 정책을 넣는 로직 수정
    아래 내용을 수정:
    - CustomerFactory에서 혜택 내역을 초기화하는 과정을 Customer로 이동
    - 혜택 받는 이벤트를 저장하는 의미로, benefitsEvents로 변수명 수정

--- src/main/java/christmas/model/Customer.java
+++ src/main/java/christmas/model/Customer.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import christmas.model.policy.EventPolicyCategory;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,23 +9,34 @@ import java.util.stream.Collectors;
 public class Customer {
 
     private final Orders orders;
-    private final Set<EventPolicyCategory> eventPolicies;
+    private final Set<EventPolicyCategory> benefitEvents;
 
-    public Customer(Orders orders, Set<EventPolicyCategory> eventPolicies) {
-        this.eventPolicies = eventPolicies;
+    public Customer(Orders orders) {
+        this.benefitEvents = createBenefits(orders);
         this.orders = orders;
+    }
+
+    private Set<EventPolicyCategory> createBenefits(Orders orders) {
+        Set<EventPolicyCategory> categories = new HashSet<>();
+        for (EventPolicyCategory eventCategory: EventPolicyCategory.values()) {
+            Integer discountPrice = eventCategory.calculateDiscountPrice(orders);
+            if (discountPrice != 0) {
+                categories.add(eventCategory);
+            }
+        }
+        return categories;
     }
 
     public Integer getTotalDiscountPrice() {
         int totalDiscountPrice = 0;
-        for (EventPolicyCategory event : eventPolicies) {
+        for (EventPolicyCategory event : benefitEvents) {
             totalDiscountPrice += event.calculateDiscountPrice(orders);
         }
         return totalDiscountPrice;
     }
 
     public Map<String, Integer> getBenefitDetails() {
-        return eventPolicies.stream()
+        return benefitEvents.stream()
                 .collect(Collectors.toMap(EventPolicyCategory::getEventName,
                         eventPolicyCategory -> eventPolicyCategory.calculateDiscountPrice(orders)));
     }
