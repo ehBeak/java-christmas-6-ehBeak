e6b2140dfeee5702a600e3fcb189a58ee657e48f
eunhee <dmsgml9903@gmail.com>
2023-11-12 04:58:16 +0900

    test(CustomerTest): 주문한 내역에 따라 할인 내역과 전체 할인 금액을 반환하는 기능 테스트
    아래 내용을 테스트:
    - 사용자의 주문 내역에 대한 전체 할인 금액을 반환하는 로직 테스트
    - 사용자의 주문 내역에 대한 할인 내역을 반환하는 로직 테스트

---
+++ src/test/java/christmas/model/CustomerTest.java
@@ -0,0 +1,51 @@
+package christmas.model;
+
+import static christmas.model.menu.Menu.BUTTON_MUSHROOM_SOUP;
+import static christmas.model.menu.Menu.RED_WINE;
+import static christmas.model.menu.Menu.T_BONE_STEAK;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.model.menu.Menu;
+import java.time.LocalDate;
+import java.util.EnumMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+
+    @DisplayName("사용자의 전체 할인 금액을 반환한다.")
+    @Test
+    void getTotalDiscountPrice() {
+        LocalDate orderDate = LocalDate.of(2023, 12, 3);
+        Orders validOrders = createValidOrders(orderDate);
+        Customer customer = new Customer(validOrders);
+
+        Integer totalDiscountPrice = customer.getTotalDiscountPrice();
+
+        assertThat(totalDiscountPrice).isEqualTo(-27200);
+    }
+
+    @DisplayName("혜택을 받는 이벤트 명과 할인 금액을 반환한다.")
+    @Test
+    void getBenefitsDetails() {
+        LocalDate orderDate = LocalDate.of(2023, 12, 3);
+        Orders validOrders = createValidOrders(orderDate);
+        Customer customer = new Customer(validOrders);
+
+        Map<String, Integer> benefitDetails = customer.getBenefitDetails();
+
+        assertThat(benefitDetails).containsOnly(
+                Map.entry("크리스마스 디데이 할인", -1200),
+                Map.entry("증정 이벤트", -25000),
+                Map.entry("특별 할인", -1000));
+    }
+
+    Orders createValidOrders(LocalDate orderDate) {
+        Map<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+        orderMenus.put(BUTTON_MUSHROOM_SOUP, 2);
+        orderMenus.put(RED_WINE, 1);
+        orderMenus.put(T_BONE_STEAK, 1);
+        return new Orders(orderMenus, orderDate);
+    }
+}
