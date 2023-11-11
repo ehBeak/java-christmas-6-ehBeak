ae13de3695926bc21b244736769ef8e553576734
eunhee <dmsgml9903@gmail.com>
2023-11-12 03:55:34 +0900

    refactor(EventPolicyCategory): 할인 이벤트명 저장하도록 수정

--- src/main/java/christmas/model/policy/EventPolicyCategory.java
+++ src/main/java/christmas/model/policy/EventPolicyCategory.java
@@ -2,19 +2,27 @@ package christmas.model.policy;
 
 public enum EventPolicyCategory {
 
-    CHRISTMAS_EVENT(new ChristmasDiscountPolicy()),
-    FREEBIES_EVENT(new FreebiesEventPolicy()),
-    SPECIAL_EVENT(new SpecialDiscountPolicy()),
-    WEEKDAY_EVENT(new WeekdayDiscountPolicy()),
-    WEEKEND_EVENT(new WeekendDiscountPolicy());
+    CHRISTMAS_EVENT("크리스마스 디데이 할인", new ChristmasDiscountPolicy()),
+    WEEKDAY_EVENT("평일 할인", new WeekdayDiscountPolicy()),
+    WEEKEND_EVENT("주말 할인", new WeekendDiscountPolicy()),
+    SPECIAL_EVENT("특별 할인", new SpecialDiscountPolicy()),
+    FREEBIES_EVENT("증정 이벤트", new FreebiesEventPolicy()),
+    ;
 
+    private final String eventName;
     private final EventPolicy eventPolicy;
 
-    EventPolicyCategory(EventPolicy eventPolicy) {
+    EventPolicyCategory(String eventName, EventPolicy eventPolicy) {
+        this.eventName = eventName;
         this.eventPolicy = eventPolicy;
     }
 
     public EventPolicy getEventPolicy() {
         return eventPolicy;
     }
+
+    public String getEventName() {
+        return eventName;
+    }
+
 }
