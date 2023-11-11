fb6a23474c3f87867a06d6419d65de35e551f717
eunhee <dmsgml9903@gmail.com>
2023-11-12 02:55:08 +0900

    feat(OutputView): 할인 전 총 주문 금액 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -9,6 +9,8 @@ public class OutputView {
     private static final String PREVIEW_MESSAGE = "%s월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_MENU_MESSAGE = "<주문메뉴>";
     private static final String ORDER_MENU_FORMAT = "%s %s개";
+    private static final String ORDER_TOTAL_PRICE_MESSAGE = "<할인 전 총주문 금액>";
+    private static final String PRICE_FORMAT = "%,d원";
 
     public void printPreviewMessage(LocalDate orderDate) {
         System.out.println(String.format(PREVIEW_MESSAGE, orderDate.getMonthValue(), orderDate.getDayOfMonth()));
@@ -18,9 +20,13 @@ public class OutputView {
         System.out.println();
         System.out.println(ORDER_MENU_MESSAGE);
         for (Menu menu : orders.keySet()) {
-            System.out.println(String.format(ORDER_MENU_FORMAT, menu, orders.get(menu)));
+            System.out.println(String.format(ORDER_MENU_FORMAT, menu.getName(), orders.get(menu)));
         }
     }
 
-
+    public void printTotalPriceBeforeDiscount(int totalPrice) {
+        System.out.println();
+        System.out.println(ORDER_TOTAL_PRICE_MESSAGE);
+        System.out.println(String.format(PRICE_FORMAT, totalPrice));
+    }
 }
