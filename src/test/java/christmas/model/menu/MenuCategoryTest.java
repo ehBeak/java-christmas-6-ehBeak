dbae1674ccf5478367b04435e3cb3a63ec35932d
eunhee <dmsgml9903@gmail.com>
2023-11-12 19:07:02 +0900

    test(MenuCategoryTest): 주어진 메뉴가 주어진 카테고리에 해당하는지 확인하는 기능 테스트

--- src/test/java/christmas/model/menu/MenuCategoryTest.java
+++ src/test/java/christmas/model/menu/MenuCategoryTest.java
@@ -1,29 +1,33 @@
 package christmas.model.menu;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.EnumSource.Mode;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class MenuCategoryTest {
 
-    @DisplayName("넘겨준 값이 음료면 true를 반환한다.")
-    @ParameterizedTest
-    @EnumSource(mode = Mode.INCLUDE, names = {"ZERO_COLA", "RED_WINE", "CHAMPAGNE"})
-    void beverageCategoryReturnTrue(Menu menu) {
-        Boolean isBeverage = MenuCategory.isBeverageCategory(menu);
-
-        assertThat(isBeverage).isTrue();
+    static Stream<Arguments> createManuAndCategory() {
+        return Stream.of(
+                arguments(Menu.BUTTON_MUSHROOM_SOUP, MenuCategory.APPETIZER, true),
+                arguments(Menu.T_BONE_STEAK, MenuCategory.MAIN, true),
+                arguments(Menu.CHAMPAGNE, MenuCategory.BEVERAGE, true),
+                arguments(Menu.BUTTON_MUSHROOM_SOUP, MenuCategory.MAIN, false),
+                arguments(Menu.CHOCOLATE_CAKE, MenuCategory.BEVERAGE, false),
+                arguments(Menu.CHAMPAGNE, MenuCategory.DESSERT, false)
+        );
     }
 
-    @DisplayName("넘겨준 값이 음료가 아니면 false를 반환한다.")
+    @DisplayName("주어진 메뉴가 주어진 카테고리에 해당하면 true를 반환하고 그렇지 않으면 false를 반환한다.")
     @ParameterizedTest
-    @EnumSource(mode = Mode.INCLUDE, names = {"T_BONE_STEAK", "CHOCOLATE_CAKE", "BUTTON_MUSHROOM_SOUP"})
-    void notBeverageCategoryReturnFalse(Menu menu) {
-        Boolean isBeverage = MenuCategory.isBeverageCategory(menu);
+    @MethodSource("createManuAndCategory")
+    void notMenuInCategoryReturnFalse(Menu menu, MenuCategory menuCategory, Boolean expected) {
+        Boolean isBeverage = MenuCategory.isMenuInCategory(menu, menuCategory);
 
-        assertThat(isBeverage).isFalse();
+        assertThat(isBeverage).isEqualTo(expected);
     }
 }
