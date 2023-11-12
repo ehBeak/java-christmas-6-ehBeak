a3bdd1aa9a4b296a26a1c9796df3a5d2a3024dd7
eunhee <dmsgml9903@gmail.com>
2023-11-12 17:19:43 +0900

    refactor(ErrorMessage): 에러 메시지를 보이는 방식 수정
    아래 내용을 수정:
    - [ERROR] 문구를 상수로 저장
    - +연산자가 아닌, join을 사용해 문자를 연결

--- src/main/java/christmas/exception/ErrorMessage.java
+++ src/main/java/christmas/exception/ErrorMessage.java
@@ -7,8 +7,7 @@ public enum ErrorMessage {
     ORDER_OVER_20_NOT_ALLOWED("메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다."),
     ORDER_UNDER_ZERO_NOT_ALLOWED("메뉴는 0개 주문할 수 없습니다."),
     ONLY_BEVERAGE_NOT_ALLOWED("음료만 주문할 수 없습니다."),
-
-    CONSECUTIVE_DELIMITER_NOT_ALLOWED("구분자가 연속될 수 없습니다. 다시 입력해 주세요.");
+    ERROR_MESSAGE("[ERROR]");
 
     ErrorMessage(String message) {
         this.message = message;
@@ -18,6 +17,6 @@ public enum ErrorMessage {
 
     @Override
     public String toString() {
-        return "[ERROR] " + message;
+        return String.join(" ", ERROR_MESSAGE.message, message);
     }
 }
