6b8721d0800139dc904d191839618efa2592ae29
eunhee <dmsgml9903@gmail.com>
2023-11-15 06:12:04 +0900

    test(DayOfWeekCategoryTest): 해당 요일이 주말인지 평일인지 판단하는 로직 테스트

--- src/main/java/christmas/model/DayOfWeekCategory.java
+++ src/main/java/christmas/model/DayOfWeekCategory.java
@@ -9,34 +9,21 @@ import static java.time.DayOfWeek.TUESDAY;
 import static java.time.DayOfWeek.WEDNESDAY;
 
 import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
 
 public enum DayOfWeekCategory {
 
-
-
     WEEK_DAYS(List.of(SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY)),
     WEEKEND_DAYS(List.of(FRIDAY, SATURDAY));
-//    WEEKEND_DAYS(List.of(FRIDAY, SATURDAY), (date -> date.getDayOfWeek().equals(SUNDAY) || date.equals(LocalDate.of(2023, 12, 25))));
 
-    private final List<DayOfWeek> dayOfWeeks; // 이걸 차라리, 일자로 묶으면 어때?
-//    private List<LocalDate> localDates;
-//    private final Predicate<LocalDate> isEventDay;
-
-    private static final LocalDate CHRISTMAS_DAY = LocalDate.of(2023, 12, 25);
+    private final List<DayOfWeek> dayOfWeeks;
 
     DayOfWeekCategory(List<DayOfWeek> dayOfWeeks) {
         this.dayOfWeeks = dayOfWeeks;
-//        this.isEventDay = isEventDay;
     }
 
     public static Boolean isDayOfWeekInCategory(DayOfWeek day, DayOfWeekCategory category) {
         return category.dayOfWeeks.stream()
                 .anyMatch(dayOfWeek -> dayOfWeek.equals(day));
     }
-
-
 }
